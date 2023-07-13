dcc9ee874c8f135c99cac9883469f3bc8d67df90
coach77777 <108881946+coach77777@users.noreply.github.com>
2023-07-13 03:42:11 -0500

    End 7

--- app.js
+++ app.js
@@ -3,9 +3,31 @@ const app = express();
 
 const dotenv = require("dotenv");
 
+const connectDatabase  = require('./config/database');
+
 // setting variables for config.env
 
-dotenv.config({ path: "./config/config.env" });
+dotenv.config({ path : "./config/config.env" });
+
+//Connecting to Database
+connectDatabase();
+
+//create
+
+//Creating own  middleware
+const middleware = (req, res, next) => {
+  console.log('Hello From Middleware.');
+
+  //Setting up user variable globally
+  req.user = 'Craig Nelson'
+  next();
+}
+app.use(middleware);
+
+//importing all routes: 
+const jobs = require('./routes/jobs')
+
+app.use ('/api/v1', jobs)
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
